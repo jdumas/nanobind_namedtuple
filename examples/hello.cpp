5d729feb44a6cb4139532201621d3f8fe8d7b300
Jérémie Dumas <jdumas@users.noreply.github.com>
2026-07-13 15:53:47 -0700

    Add compile-time field metadata and minimal tuple caster

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -1,11 +1,48 @@
+#include <string>
+
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
 
 #include <nanobind_namedtuple/named_tuple.h>
 
 namespace nb = nanobind;
 
+struct Color {
+    float r;
+    float g;
+    float b;
+};
+
+struct Point {
+    int x;
+    int y;
+    std::string label;
+};
+
+struct Empty {};
+
+NB_NAMED_TUPLE(Color, "Color", NB_NT_FIELD(r), NB_NT_FIELD(g), NB_NT_FIELD(b))
+
+NB_NAMED_TUPLE_EX(
+    Point, "Point", nbnt::field<&Point::x>("x"), nbnt::field<&Point::y>("y"),
+    nbnt::field<&Point::label>("label").default_(std::string{})
+)
+
+NB_NAMED_TUPLE(Empty, "Empty")
+
 NB_MODULE(nbnt_example_hello, m) {
     m.doc() = "Minimal nanobind extension used by the nanobind_namedtuple test suite.";
     m.def("hello", []() { return "hello from nanobind_namedtuple"; });
     m.def("add", [](int a, int b) { return a + b; });
+
+    m.def("make_color", [](float r, float g, float b) { return Color{r, g, b}; });
+    m.def("sum_color", [](Color c) { return c.r + c.g + c.b; });
+
+    m.def("make_point", [](int x, int y, std::string label) {
+        return Point{x, y, std::move(label)};
+    });
+    m.def("point_label", [](Point p) { return p.label; });
+
+    m.def("make_empty", []() { return Empty{}; });
+    m.def("take_empty", [](Empty) { return true; });
 }
